38f960a4d03a7f5ba1f008376af7ae9398847441
unknown <gabrielleon917@gmail.com>
2020-09-14 02:43:08 -0600

    strobe and color_chase animation funtions added

--- main.py
+++ main.py
@@ -9,7 +9,56 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.9, auto_write=Fal
                            pixel_order=(1, 0, 2, 3))
 
 red = (255, 0, 0, 0)
+yellow = (255, 150, 0, 0)
+green = (0, 255, 0, 0)
+cyan = (0, 255, 255, 0)
+blue = (0, 0, 255, 0)
+purple = (180, 0, 255, 0)
+white = (0, 255, 255, 255)
+off = (0, 0, 0, 0)
 
-while True:
+def strobe(wait):
 	pixels.fill(red)
 	pixels.show()
+	time.sleep(wait)
+
+	pixels.fill(yellow)
+	pixels.show()
+	time.sleep(wait)
+
+	pixels.fill(green)
+	pixels.show()
+	time.sleep(wait)
+
+	pixels.fill(cyan)
+	pixels.show()
+	time.sleep(wait)
+
+	pixels.fill(blue)
+	pixels.show()
+	time.sleep(wait)
+
+	pixels.fill(purple)
+	pixels.show()
+	time.sleep(wait)
+
+def color_chase(color, wait):
+    for i in range(num_pixels):
+		pixels[i] = color
+		time.sleep(wait)
+		pixels.show()
+
+while True:
+	color_chase(red, 0.0001)
+	time.sleep(5)
+	color_chase(yellow, 0.0001)
+	time.sleep(5)
+	color_chase(green, 0.0001)
+	time.sleep(5)
+	color_chase(cyan, 0.0001)
+	time.sleep(5)
+	color_chase(blue, 0.0001)
+	time.sleep(5)
+	color_chase(purple, 0.0001)
+	time.sleep(5)
+	strobe(0.5)
